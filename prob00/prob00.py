9d865c8a9c06ceae99be6fd9afd6a11c5ba7725e
Matt Ayers <mayers@indeed.com>
2017-03-31 16:13:48 -0700

    SOLUTION: problem 0

---
+++ prob00/prob00.py
@@ -0,0 +1,17 @@
+"""
+Problem #0 from the python challenge
+ 
+ URL: http://www.pythonchallenge.com/pc/def/0.html
+ Soln: http://www.pythonchallenge.com/pc/def/274877906944.html 
+"""
+
+def main():
+
+    result = 2 ** 38
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
+
+
