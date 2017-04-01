e8334b2f3b086b3fec3de15617450be961de3709
Matt Ayers <matt-github@coffeegrinder.org>
2017-04-01 13:41:21 -0700

    prob7 - initial framework

---
+++ prob07/prob07.py
@@ -0,0 +1,23 @@
+"""
+prob07
+
+ok, so running down the middle of this image is a set of grey bars. 
+time to poke around the image and see what the values are
+
+URL: http://www.pythonchallenge.com/pc/def/oxygen.html
+Soln: 
+"""
+
+import pillow
+import urllib
+
+def main():
+    """
+    """
+
+    image_url = 'http://www.pythonchallenge.com/pc/def/oxygen.png'
+
+
+
+if __name__ == '__main__':
+    main()
