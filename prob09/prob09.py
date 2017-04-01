9b416b82d425fc7a18b7774e766597155ba46594
Matt Ayers <matt-github@coffeegrinder.org>
2017-04-01 14:31:33 -0700

    problem 09 - scaffolding

---
+++ prob09/prob09.py
@@ -0,0 +1,17 @@
+"""
+prob09
+
+URL: http://www.pythonchallenge.com/pc/return/good.html
+Soln:
+"""
+
+
+def main():
+    """
+    ok, so there's a tree
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
