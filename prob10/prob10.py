d979f8c8f1b9a25c05421cd051e00d0e4e2fdf72
Matt Ayers <matt-github@coffeegrinder.org>
2017-04-01 18:47:42 -0700

    SOLUTION: solved problem 10 - auto pep

--- prob10/prob10.py
+++ prob10/prob10.py
@@ -18,11 +18,12 @@ URL: http://www.pythonchallenge.com/pc/return/bull.html
 Soln: http://www.pythonchallenge.com/pc/return/5808.html
 """
 
+
 def get_next_in_seq(value_string):
     """
     ok, so this will count the occurances of a digit until it changes.
     It then records the count and digit in a tuple
-    
+
     :param value_string: the initial value
     :return: the set of ordered counts - length = # of changes + 1
     """
@@ -38,20 +39,21 @@ def get_next_in_seq(value_string):
             # fun fact - if you don't reset the count,
             # you turn your laptop into a space heater (oops)
         cur_char = next_char
-    
+
     # and take care of the last ones
     result_arr.append((count, cur_char))
-    
+
     return result_arr
+
 
 def count_array_to_string(count_array):
     """
     takes an array of tuples of the form [(1,2), (2,1)] and creates a string "1221"
-    
+
     [(1,'1')] -> "11"
     [(2,'1')] -> "21"
     [(1, '2'), (1, '1')] -> "1211"
-    
+
     :param count_array: the array of tuples
     :return: the string
     """
@@ -62,18 +64,20 @@ def count_array_to_string(count_array):
 
     return retval
 
+
 def main():
     """
     compute the length of the 1, 11, 21, 1211 series
     """
     next_val_string = '1'
-    
+
     for counter in range(0, 31):
         print("{}:\t[{}]".format(counter, len(next_val_string)))
         next_val_list = get_next_in_seq(next_val_string)
         next_val_string = count_array_to_string(next_val_list)
 
     # and so it ends with 5808
+
 
 if __name__ == '__main__':
     main()
