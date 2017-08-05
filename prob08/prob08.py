0727e03384b4b7b2831c15ccad8962fc79e207bc
Matt Ayers <matt-github@coffeegrinder.org>
2017-08-04 17:34:32 -0700

    cleaning up

--- prob08/prob08.py
+++ prob08/prob08.py
@@ -10,7 +10,6 @@ BZ + the bee image == bzip? We'll try that, first
 
 URL: http://www.pythonchallenge.com/pc/def/integrity.html
 Soln: http://www.pythonchallenge.com/pc/def/integrity.html (user: huge, pass: file)
-      
 """
 
 import bz2
@@ -19,19 +18,21 @@ def main():
     """
     let's bunzip the username and password, above
     """
-    user_bz = b'BZh91AY&SYA\xaf\x82\r\x00\x00\x01\x01\x80\x02\xc0\x02\x00 \x00!\x9ah3M\x07<]\xc9\x14\xe1BA\x06\xbe\x084'
-    user_pw = b'BZh91AY&SY\x94$|\x0e\x00\x00\x00\x81\x00\x03$ \x00!\x9ah3M\x13<]\xc9\x14\xe1BBP\x91\xf08'
+    user_bz = \
+        b'BZh91AY&SYA\xaf\x82\r\x00\x00\x01\x01\x80\x02\xc0\x02\x00 \x00!\x9ah3M\x07<]\xc9\x14\xe1BA\x06\xbe\x084'
+    user_pw = \
+        b'BZh91AY&SY\x94$|\x0e\x00\x00\x00\x81\x00\x03$ \x00!\x9ah3M\x13<]\xc9\x14\xe1BBP\x91\xf08'
 
     user_string = bz2.decompress(user_bz)
     pw_string = bz2.decompress(user_pw)
 
-    print(f"USER: {user_string}")
-    print(f"PASS: {pw_string}")
+    print("USER: {}".format(user_string))
+    print("PASS: {}".format(pw_string))
 
     # this gives us "huge" and "file" -- something tells me that there's more to it
-
     # nope. wow.
 
+    print("done")
 
 if __name__ == '__main__':
     main()
