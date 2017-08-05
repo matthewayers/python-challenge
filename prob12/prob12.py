2df26003ca572f3e00a3aafeec278a7223c2ac54
Matt Ayers <matt-github@coffeegrinder.org>
2017-08-04 17:34:57 -0700

    initial commit

---
+++ prob12/prob12.py
@@ -0,0 +1,18 @@
+"""
+prob 12
+dealing evil
+
+another blurry picture - maybe another even odd problem?
+
+"""
+
+
+def main():
+    """
+    read in the image and create two images - one w/even pixels, one w/ odd
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
