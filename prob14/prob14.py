023e4d7de4a5a94493441f88870b1cda904a8d27
Matt Ayers <matt-github@coffeegrinder.org>
2017-12-18 22:23:19 -0700

    initial commit

---
+++ prob14/prob14.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+prob 14
+walk around
+
+picture of a cinnamon roll; weird lined box at the bottom
+
+
+URL: http://www.pythonchallenge.com/pc/return/italy.html
+soln:
+"""
+
+
+def main():
+    """
+    uhhh... not sure on this one; time to start looking
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
